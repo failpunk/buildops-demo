80dd2dfb26f955da287dd32ab969d16ee6bf9e16
Justin Vencel <failpunk@gmail.com>
2019-07-31 16:50:35 -0700

    Updating key

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://njkct2x7mvfgpnkfbmlerbibsi.appsync-api.us-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-qc2tyxbgivbkhnm54vms5czsba"
+    "aws_appsync_apiKey": "da2-b452dzprefbqjblcqv22ekzeh4"
 };
 
 
